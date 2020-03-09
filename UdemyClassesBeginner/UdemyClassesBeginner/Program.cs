88caa10cfb4fb632405f4cf0c324e62cfdbbad95
Lezakk <wiktro.zak94@gmail.com>
2020-03-09 22:15:34 +0100

    First commit

---
+++ UdemyClassesBeginner/UdemyClassesBeginner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdemyClassesBeginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
