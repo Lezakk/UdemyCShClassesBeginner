5524f08cb9a1df25cf13791447f02daafcbda5f1
Lezakk <wiktro.zak94@gmail.com>
2020-03-09 22:44:30 +0100

    Add NUnit with test class

--- UdemyClassesBeginner/UdemyClassesBeginner/Test.cs
+++ UdemyClassesBeginner/UdemyClassesBeginner/Test.cs
@@ -1,15 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace UdemyClassesBeginner
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-
     public class Test
     {
         public void Siema()
---
+++ UdemyClassesBeginner/UdemyClassesBeginner_Tests/Test_Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using UdemyClassesBeginner;
+
+namespace UdemyClassesBeginner_Tests
+{
+    class Test_Tests
+    {
+        [Test]
+        public void Test()
+        {
+            var test = new Test();
+        }
+    }
+}
