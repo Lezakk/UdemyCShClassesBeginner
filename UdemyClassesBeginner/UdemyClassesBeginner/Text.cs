91502eeee71965bf509074e80805f3e4d8c3854d
Lezakk <wiktro.zak94@gmail.com>
2020-03-15 20:25:55 +0100

    Add Text class and TextTests class, add testing method for Consecutive method

---
+++ UdemyClassesBeginner/UdemyClassesBeginner/Text.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyClassesBeginner
+{
+    public class Text
+    {
+        public string Consecutive(string text)
+        {
+
+        }
+    }
+}
---
+++ UdemyClassesBeginner/UdemyClassesBeginner_Tests/TextTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyClassesBeginner;
+using NUnit.Framework;
+
+namespace UdemyClassesBeginner_Tests
+{
+    [TestFixture]
+    class TextTests
+    {
+        [TestCase("1-2-3-4-5")]
+        [TestCase("9-8-7-6-5")]
+        public void Text_Consecutive_WhenConsecutiveStringPassed_ReturnsMessageConsecutive(string consecutiveText)
+        {
+            var text = new Text();
+            var result = text.Concecutive(consecutiveText);
+            Assert.That(result, Does.Contain("Consecutive"));
+        }
+    }
+}
